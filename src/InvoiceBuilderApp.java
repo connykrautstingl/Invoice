83b99ef348bc8f48e3c22c0f7b0052a26324504b
Connykrautstingl <s44784@edu.campus02.at>
2021-10-09 11:13:34 +0200

    Initial commit

---
+++ src/InvoiceBuilderApp.java
@@ -0,0 +1,19 @@
+public class InvoiceBuilderApp {
+
+    public static void main(String[] args) {
+        double article1 = 0;
+        double article2 = 0;
+        double article3 = 0;
+        double article4 = 0;
+        double article5 = 0;
+
+        article1 = 2.4; //Kaffee
+        article2 = 2.0; //Kipferl
+        article3 = 2.9; //Joghurt
+
+        double summe = article1 + article2 + article3 + article4 + article5;
+
+        System.out.println("Gesamtkosten:" + summe);
+
+    }
+}
